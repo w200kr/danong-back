21bd8d2658ba07d14873d1becd9007b500781dd9
Asin <w200kr@gmail.com>
2020-10-21 23:17:54 +0900

    env_crawl.py 주석

--- env_crawl.py
+++ env_crawl.py
@@ -18,18 +18,21 @@ import time
 import json
 import pandas as pd
 
+# 크롤링할 농사로 서비스 url
 base_url = 'http://www.nongsaro.go.kr/portal/ps/psa/psab/psabe/openApiSoilVrifyLst.ps?menuId=PS03329'
-
+# 농지 주소
 location_text = '서울특별시 강남구 논현동 69'
-
+# 공백으로 농지 주소를 나눈다. ['서울특별시', '강남구', '논현동', '69]'
 location_texts = location_text.split()
 
-# exclude 리
+# '리' 제외 
 if len(location_texts)==4:
     [sido, sigungu, townmyeondong, jibn] = location_texts
+# '리' 포함 ex) 경기도 남양주시 퇴계원면 ~~리 12-34
 elif len(location_texts)==5:
     [sido, sigungu, townmyeondong, ri, jibn] = location_texts
 
+# 크롬 브라우저 이용해서 크롤링을 수행한다.
 def webdriver_init(base_url):
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
@@ -40,15 +43,28 @@ def webdriver_init(base_url):
 
 driver = webdriver_init(base_url)
 
+
+# 시/도 선택항을 브라우저 상에서 가져온다.
 sido_select = Select(driver.find_element_by_id('siDoLst'))
+# location_text의 '서울특별시'를 선택한다. 
 sido_select.select_by_visible_text(sido)
 
+# sido_select 값이 설정되면 시/군/구 선택항의 값이 세팅될때까지 시간이 걸린다.
+# 해당 시간을 최대 10초 기다린다. 그전에 시/군/구 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
 sigungu_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="siGunGuLst"]/option[2]')))
+# 시/군/구 선택항을 브라우저 상에서 가져온다.
 sigungu_select = Select(sigungu_option.find_element_by_xpath('..'))
+# location_text의 '강남구'를 선택한다. 
 sigungu_select.select_by_visible_text(sigungu)
 
+# 이하 반복
+
+
+# 해당 시간을 최대 10초 기다린다. 그전에 읍/면/동 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
 townmyeondong_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="townMyeonDongLst"]/option[2]')))
+# 읍/면/동 선택항을 브라우저 상에서 가져온다.
 townmyeondong_select = Select(townmyeondong_option.find_element_by_xpath('..'))
+# location_text의 '논현동'을 선택한다. 
 townmyeondong_select.select_by_visible_text(townmyeondong)
 
 try:
