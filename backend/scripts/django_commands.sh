f225fb9fbc9f508dd68e8caf72c4277af33763d1
Asin <w200kr@gmail.com>
2020-11-14 21:01:32 +0900

    add collectstatic line

--- backend/scripts/django_commands.sh
+++ backend/scripts/django_commands.sh
@@ -2,4 +2,4 @@
 
 # sudo /home/bitnami/backend/manage.py migrate --settings backend.settings.production
 # sudo /home/bitnami/backend/manage.py set_su --settings backend.settings.production
-# sudo /home/bitnami/backend/manage.py collectstatic --settings backend.settings.production --noinput
+sudo /home/bitnami/backend/manage.py collectstatic --settings backend.settings.production --noinput
