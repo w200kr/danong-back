39c2e1c1676302e3ab3379bf26c844cd8ed2f21e
Asin <w200kr@gmail.com>
2020-11-14 19:41:12 +0900

    fix db settings

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -20,7 +20,8 @@ ALLOWED_HOSTS += ['localhost', '127.0.0.1',]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "danong-db",
+        # 'NAME': "danong-db",
+        'NAME': "dbmaster",
         'HOST': "ls-5db51d29d29b23f6ec09ad79b01329c88317ebff.ckt4xrz0zrhs.ap-northeast-2.rds.amazonaws.com",
         'PORT': 3306,
         'USER': "asin",
