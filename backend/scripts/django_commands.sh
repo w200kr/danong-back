b71f3b016c354636057cf678e42809d3f85f2feb
Asin <w200kr@gmail.com>
2020-11-14 21:12:47 +0900

    migrate set_su collectstatic

--- backend/scripts/django_commands.sh
+++ backend/scripts/django_commands.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 cd /home/bitnami/backend
-
 sudo ./manage.py migrate --settings backend.settings.production
-# sudo /home/bitnami/backend/manage.py set_su --settings backend.settings.production
-# sudo /home/bitnami/backend/manage.py collectstatic --settings backend.settings.production --noinput
+sudo ./manage.py set_su --settings backend.settings.production
+sudo ./manage.py collectstatic --settings backend.settings.production --noinput
