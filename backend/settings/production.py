5333a831efa6b9c092f87334ce32a5d6db390ed4
Asin <w200kr@gmail.com>
2020-11-14 19:19:13 +0900

    fix db settings

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -21,7 +21,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "danong-db",
-        'HOST': "http://ls-5db51d29d29b23f6ec09ad79b01329c88317ebff.ckt4xrz0zrhs.ap-northeast-2.rds.amazonaws.com",
+        'HOST': "ls-5db51d29d29b23f6ec09ad79b01329c88317ebff.ckt4xrz0zrhs.ap-northeast-2.rds.amazonaws.com",
         'PORT': 3306,
         'USER': "asin",
         'PASSWORD': "#|Z:#8]sT6NQF#E2=dIWk-.48ghLe!9=",
