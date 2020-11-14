8ea2f1468b9faf204114627255261fb7c4336194
Asin <w200kr@gmail.com>
2020-11-14 20:48:53 +0900

    add pip install

--- backend/scripts/django_commands.sh
+++ backend/scripts/django_commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo /home/bitnami/backend/manage.py migrate --settings backend.settings.production
-sudo /home/bitnami/backend/manage.py set_su --settings backend.settings.production
-sudo /home/bitnami/backend/manage.py collectstatic --settings backend.settings.production --noinput
+# sudo /home/bitnami/backend/manage.py migrate --settings backend.settings.production
+# sudo /home/bitnami/backend/manage.py set_su --settings backend.settings.production
+# sudo /home/bitnami/backend/manage.py collectstatic --settings backend.settings.production --noinput
--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python /opt/bitnami/python/bin/pip install --upgrade pip
+pip install -r /home/bitnami/backend/requirements.txt
+# python /opt/bitnami/python/bin/pip install --upgrade pip
 # sudo pip install -r requirements.txt
