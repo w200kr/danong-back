69063fce29235643e856aa3449a00e7eaeedb2e1
Asin <w200kr@gmail.com>
2020-11-14 19:56:15 +0900

    dd

--- backend/scripts/django_commands.sh
+++ backend/scripts/django_commands.sh
@@ -1,3 +1,3 @@
 sudo ./manage.py migrate --settings backend.settings.production
 sudo ./manage.py set_su --settings backend.settings.production
-sudo ./manage.py collectstatic --settings backend.settings.production
+sudo ./manage.py collectstatic --settings backend.settings.production --noinput
