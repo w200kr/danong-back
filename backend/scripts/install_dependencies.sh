b8fd51bb8d31d947b5c6318989cc09f941c54e85
Asin <w200kr@gmail.com>
2020-11-14 21:15:21 +0900

    pip upgrade install requirements.txt

--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# apt-get check
-# python /opt/bitnami/python/bin/pip install --upgrade pip
-# sudo pip install -r requirements.txt
+cd /home/bitnami/backend
+sudo pip install --upgrade pip
+sudo pip install -r requirements.txts
