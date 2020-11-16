a9d818496644931b4f52aca532c10f8703fdf60f
Asin <w200kr@gmail.com>
2020-11-17 04:55:21 +0900

    crawler class

--- api/crawls/__init__.py
+++ api/crawls/__init__.py
@@ -1,137 +1,140 @@
-#-*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-# from bs4 import BeautifulSoup as bs
-from IPython import embed
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as bs
 
+from IPython import embed
 
 import requests
-import re
-import datetime
-import time
-import json
-# import pandas as pd
+import random
+import os
+
+base_url = 'http://www.nongsaro.go.kr/portal/ps/psa/psab/psabe/openApiSoilVrifyLst.ps?menuId=PS03329'
+
+class Crawler(object):
+    """docstring for Crawler"""
+    def __init__(self):
+        super(Crawler, self).__init__()
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--ignore-certificate-errors')
+
+        self.driver = webdriver.Chrome(os.path.dirname(os.path.abspath(__file__))+"/chromedriver", options=options)
+        self.driver.get(base_url)
+
+        self.sido_select = Select(self.driver.find_element_by_id('siDoLst'))
+        self.sigungu_select = Select(self.driver.find_element_by_id('siGunGuLst'))
+        self.townMyeonDong_select = Select(self.driver.find_element_by_id('townMyeonDongLst'))
+        self.ri_select = Select(self.driver.find_element_by_id('riLst'))
+        self.jibn_select = Select(self.driver.find_element_by_id('sel_jibn'))
+
+        self.geocode_url = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="
+        self.headers = {
+            'X-NCP-APIGW-API-KEY-ID': 'm11ogby6ag',
+            'X-NCP-APIGW-API-KEY': 'G8nc8zH5sP4pg8ZVMYETnLoReXCfx04vgNKvwsPE',
+        }
+
+    def select_by_text(self, select, text):
+        if text is None or len(text)==0:
+            return 
+        text = text.replace(' ', '')
+
+        for index, option in enumerate(select.options):
+            if text in option.text:
+                select.select_by_index(index)
+                self.wait_for_load()
+                return 
+
+    def select_by_random(self, select):
+        if len(select.options) > 1:
+            random_int = random.randint(1, len(select.options)-1)
+            select.select_by_index(random_int)
+            self.wait_for_load()
+
+    def wait_for_load(self):
+        driver = self.driver
+        WebDriverWait(driver, 10).until(EC.staleness_of(driver.find_element_by_css_selector('div.blockPage')))
+        
+    def remove_empty_objs(self, unfiltered_list):
+        return list(filter(lambda info: len(info['longName'])!=0, unfiltered_list))
+
+    def find_obj(self, target_list, key, value):
+        return next( (obj for obj in target_list if obj[key][0]==value), None)
+
+    def get_value(self, obj, key):
+        if obj is not None:
+            return obj[key]
+
+    # def get_key_by_value(self, obj, value):
+    #     return [ obj]
+
+    # def remove_blank(self, string):
+    #     return string.replace(' ', '')
+
+    def lookup_geocode(self, address):
+        url = self.geocode_url+address
+        response = requests.get(url, headers=self.headers)
+        response_json = response.json()
+
+        # return {
+        #     'lng': response['addresses'][0]['x'],
+        #     'lat': response['addresses'][0]['y']
+        # }
+        if response.status_code == 200 and response_json['status'] == 'OK':
+            if response_json['meta']['totalCount']>0:
+                return response_json['addresses'][0]
+            else:
+                raise Exception('검색 결과가 없습니다.')
+        else:
+            raise Exception('정상적인 응답을 받지 못했습니다.')
 
 
+    # step 1. 입력된 주소값을 Naver Geocode API로 조회
+    # step 2. 정상적으로 조회된 결과가 존재하면 해당 지번 주소를 농사로 페이지에 입력 후 결과를 크롤링
+    def lookup_nongsaro(self, address):
+        driver = self.driver
+        try:
+            searched_address = self.lookup_geocode(address)
+            geo_position = {
+                'lng': searched_address['x'],
+                'lat': searched_address['y']
+            }
+            address_elements = searched_address['addressElements']
+        except Exception as e:
+            raise e
 
-# 크롬 브라우저 이용해서 크롤링을 수행한다.
-def webdriver_init(base_url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome("./chromedriver", options=options)
-    # driver = webdriver.Chrome('./chromedriver')
-    driver.get(base_url)
-    return driver
+        filtered_list = self.remove_empty_objs(address_elements)
 
+        sido_text = self.get_value( self.find_obj(filtered_list, 'types', 'SIDO'), 'longName' )
+        sigungu_text = self.get_value( self.find_obj(filtered_list, 'types', 'SIGUGUN'), 'longName' )
+        townMyeonDong_text = self.get_value( self.find_obj(filtered_list, 'types', 'DONGMYUN'), 'longName' )
+        ri_text = self.get_value( self.find_obj(filtered_list, 'types', 'RI'), 'longName' )
+        jibn_text = self.get_value( self.find_obj(filtered_list, 'types', 'LAND_NUMBER'), 'longName' )
 
-# def get_random_addresses(addresses_num):
-#     base_url = 'http://www.nongsaro.go.kr/portal/ps/psa/psab/psabe/openApiSoilVrifyLst.ps?menuId=PS03329'
+        self.select_by_text(self.sido_select, sido_text)
+        self.select_by_text(self.sigungu_select, sigungu_text)
+        self.select_by_text(self.townMyeonDong_select, townMyeonDong_text)
+        self.select_by_text(self.ri_select, ri_text)
+        self.select_by_text(self.jibn_select, jibn_text)
 
-#     driver = webdriver_init(base_url)
-
+        address = driver.find_element_by_xpath('//*[@id="jibun_div"]/div[2]/table/tbody/tr/td').text
+        aptitude_div = driver.find_element_by_xpath('//*[@id="jibun_div"]/div[4]')
+        soup = bs(aptitude_div.get_attribute('innerHTML'), features="html.parser")
     
+        response = {
+            'address': searched_address['jibunAddress'],
+            'aptitude_table': str(soup).replace('\n', ''),
+            **geo_position,
+        }
 
-    # 시/도 선택항을 브라우저 상에서 가져온다.
-    sido_select = Select(driver.find_element_by_id('siDoLst'))
-    # location_text의 '서울특별시'를 선택한다. 
-    sido_select.select_by_visible_text(sido)
-
-    # sido_select 값이 설정되면 시/군/구 선택항의 값이 세팅될때까지 시간이 걸린다.
-    # 해당 시간을 최대 10초 기다린다. 그전에 시/군/구 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
-    sigungu_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="siGunGuLst"]/option[2]')))
-    # 시/군/구 선택항을 브라우저 상에서 가져온다.
-    sigungu_select = Select(sigungu_option.find_element_by_xpath('..'))
-    # location_text의 '강남구'를 선택한다. 
-    sigungu_select.select_by_visible_text(sigungu)
-
-    # 이하 반복
-
-
-    # 해당 시간을 최대 10초 기다린다. 그전에 읍/면/동 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
-    townmyeondong_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="townMyeonDongLst"]/option[2]')))
-    # 읍/면/동 선택항을 브라우저 상에서 가져온다.
-    townmyeondong_select = Select(townmyeondong_option.find_element_by_xpath('..'))
-    # location_text의 '논현동'을 선택한다. 
-    townmyeondong_select.select_by_visible_text(townmyeondong)
-
-
-
-
-# # 크롤링할 농사로 서비스 url
-# base_url = 'http://www.nongsaro.go.kr/portal/ps/psa/psab/psabe/openApiSoilVrifyLst.ps?menuId=PS03329'
-# # 농지 주소
-# location_text = '서울특별시 강남구 논현동 69'
-# # 공백으로 농지 주소를 나눈다. ['서울특별시', '강남구', '논현동', '69]'
-# location_texts = location_text.split()
-
-# # '리' 제외 
-# if len(location_texts)==4:
-#     [sido, sigungu, townmyeondong, jibn] = location_texts
-# # '리' 포함 ex) 경기도 남양주시 퇴계원면 ~~리 12-34
-# elif len(location_texts)==5:
-#     [sido, sigungu, townmyeondong, ri, jibn] = location_texts
-
-
-# driver = webdriver_init(base_url)
-
-
-# # 시/도 선택항을 브라우저 상에서 가져온다.
-# sido_select = Select(driver.find_element_by_id('siDoLst'))
-# # location_text의 '서울특별시'를 선택한다. 
-# sido_select.select_by_visible_text(sido)
-
-# # sido_select 값이 설정되면 시/군/구 선택항의 값이 세팅될때까지 시간이 걸린다.
-# # 해당 시간을 최대 10초 기다린다. 그전에 시/군/구 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
-# sigungu_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="siGunGuLst"]/option[2]')))
-# # 시/군/구 선택항을 브라우저 상에서 가져온다.
-# sigungu_select = Select(sigungu_option.find_element_by_xpath('..'))
-# # location_text의 '강남구'를 선택한다. 
-# sigungu_select.select_by_visible_text(sigungu)
-
-# # 이하 반복
-
-
-# # 해당 시간을 최대 10초 기다린다. 그전에 읍/면/동 선택항의 선택지가 로딩되면 다음 동작을 실행한다.
-# townmyeondong_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="townMyeonDongLst"]/option[2]')))
-# # 읍/면/동 선택항을 브라우저 상에서 가져온다.
-# townmyeondong_select = Select(townmyeondong_option.find_element_by_xpath('..'))
-# # location_text의 '논현동'을 선택한다. 
-# townmyeondong_select.select_by_visible_text(townmyeondong)
-
-# try:
-#     ri
-#     ri_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="riLst"]/option[2]')))
-#     ri_select = Select(townmyeondong_option.find_element_by_xpath('..'))
-#     ri_select.select_by_visible_text(townmyeondong)
-# except NameError:
-#     pass
-
-# jibn_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="sel_jibn"]/option[2]')))
-# jibn_select = Select(jibn_option.find_element_by_xpath('..'))
-# soup = bs(driver.page_source, 'html.parser')
-# select_soup = soup.find('select',{'id':'sel_jibn'})
-# jibn_select.select_by_visible_text(select_soup.find(text=re.compile(jibn)))
-
-# result_div = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="jibun_div"]')))
-# soup = bs(driver.page_source, 'html.parser')
-# result_div_html = soup.find('div', {'id': 'jibun_div'})
-
-# print result_div_html
-
-# # embed()
-
-# driver.quit()
-
-# # TODO : 세종시 예외
-# # TODO : 주소 일치하지 않을 경우 혹은 명칭이 다를 경우(서울특별시 or 서울시)
-
-try:
-    pass
-except Exception as e:
-    raise e
+        return response
