e7341ac9815138d9cea77d0710e3738b17b4defc
Asin <w200kr@gmail.com>
2020-11-24 14:53:35 +0900

    fix get_or_create

--- api/management/commands/set_su.py
+++ api/management/commands/set_su.py
@@ -10,24 +10,23 @@ class Command(BaseCommand):
         user_model = get_user_model()
         asin, created = user_model.objects.get_or_create(
             username="asin", 
-            email="w200kr@gmail.com", 
-            is_staff=True,
-            is_superuser=True,
-            # category='Player',
-            # student_id='2012920031',
-            # tel='01092874497',
-            # name='신석은',
-        )
-        asin_profile, created = Profile.objects.get_or_create(
-            user=asin,
-            category='S',
-            name='신석은',
-            tel='92874497',
-            address='서울특별시 동대문구 망우로12길 49-1',
-            address_detail='지하 B102호',
-            kakao_id='1520435556'   
         )
 
+        asin.email="w200kr@gmail.com"
+        asin.is_staff=True
+        asin.is_superuser=True
+
+        asin_profile, created = Profile.objects.get_or_create(
+            user=asin
+        )
+        asin_profile.category='S'
+        asin_profile.name='신석은'
+        asin_profile.tel='92874497'
+        asin_profile.address='서울특별시 동대문구 망우로12길 49-1'
+        asin_profile.address_detail='지하 B102호'
+        asin_profile.kakao_id='1520435556'
+
         asin.set_password('hawks2012')
+
         asin.save()
         asin_profile.save()
