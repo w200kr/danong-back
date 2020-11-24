12121dc607a808494b3dd961ff8368a55891d109
Asin <w200kr@gmail.com>
2020-11-24 21:31:04 +0900

    dfs

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import redirect
 from rest_framework import mixins, generics, status, filters
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 # from rest_framework import mixins, generics, viewsets
 from rest_framework.response import Response
